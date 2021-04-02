1f21b85e6d7f5dcf07fa0e32a22538789a7123ab
Hector <hectorvives@gmail.com>
2021-04-02 17:27:51 -0500

    env

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ require('dotenv').config({ path: 'variables.env' });
 
 const conectarDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_MONGO, {
+        //await mongoose.connect(process.env.DB_MONGO, {
+        await mongoose.connect('mongodb://proyectoarqcloud02:covid2021@cluster-documentdb-upc2021.cluster-cvikowjpazmy.us-east-1.docdb.amazonaws.com:27017/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
