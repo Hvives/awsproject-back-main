49b4c00febaef275d0f4a2204ecc5f632861c0a8
Hector <hectorvives@gmail.com>
2021-04-02 20:39:12 -0500

    env

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ const conectarDB = async () => {
     try {
         //await mongoose.connect(process.env.DB_MONGO, {
         await mongoose.connect('mongodb://proyectoarqcloud02:covid2021@cluster-documentdb-upc2021.cluster-cvikowjpazmy.us-east-1.docdb.amazonaws.com:27017/test?retryWrites=true&w=majority', {
+            useCreateIndex: true,
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
