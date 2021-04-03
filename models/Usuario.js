8028691ac8b1cd15ef954cf0f1d5ae5513040ce2
Hector <hectorvives@gmail.com>
2021-04-02 20:43:33 -0500

    env

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const UsuariosSchema = mongoose.Schema({
+const UsuariosSchema = mongoose.model('Usuarios', new Schema({
     nombre: {
         type: String,
         required: true,
@@ -21,6 +21,6 @@ const UsuariosSchema = mongoose.Schema({
         type: Date,
         default: Date.now()
     }
-});
+}));
 
 module.exports = mongoose.model('Usuario', UsuariosSchema);
