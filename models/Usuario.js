a16a3cebc178887cd0b1a02c111a79e86d23a97c
Hector <hectorvives@gmail.com>
2021-04-02 20:44:41 -0500

    env

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const UsuariosSchema = mongoose.model('Usuarios', new Schema({
+const UsuariosSchema = mongoose.model('Usuarios', mongoose.Schema({
     nombre: {
         type: String,
         required: true,
